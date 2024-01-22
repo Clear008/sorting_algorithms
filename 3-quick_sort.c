33bd7829f5becb236b7f7568f40c002a1a44f366
Clear008 <wissal-exo@hotmail.com>
2024-01-22 14:07:43 -0800

    3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,9 +1,10 @@
 #include "sort.h"
 
 
-int l_partition(int *array, int low, int high, size_t size);
+int lomuto_part(int *array, int low, int high, size_t size);
 /**
  * quick_sort - Sorts an array of integers in ascending order
+ * 
  * @array: An array of numbers.
  * @size: The size of the array.
  */
@@ -24,45 +25,48 @@ void quicksort_recursive(int *array, int low, int high, size_t size)
 {
 if (low < high)
 {
-int partition_index = l_partition(array, low, high, size);
+int partition_index = lomuto_part(array, low, high, size);
 
 quicksort_recursive(array, low, partition_index - 1, size);
 quicksort_recursive(array, partition_index + 1, high, size);
 }
 }
 /**
- * l_partition - Implements the Lomuto partition scheme.
+ * lomuto_part - Implements the Lomuto partition scheme.
  * @array: The array of numbers.
  * @low: The low index of the partition.
  * @high: The high index of the partition.
  * @size: The size of the array.
  * Return: The partition index.
  */
-int l_partition(int *array, int low, int high, size_t size)
+int lomuto_part(int *array, int low, int high, size_t size)
 {
 int pivot = array[high];
 int i = low - 1;
-int temp, j;
+int j, temp;
 
 for (j = low; j <= high - 1; j++)
 {
-if (array[j] < pivot)
+if (array[j] <= pivot)
 {
 i++;
+if (i != j)
+{
 temp = array[i];
 array[i] = array[j];
 array[j] = temp;
-
 print_array(array, size);
 }
 }
+}
 
+if (i + 1 != high)
+{
 temp = array[i + 1];
 array[i + 1] = array[high];
 array[high] = temp;
-
 print_array(array, size);
-
+}
 return (i + 1);
 }
 
