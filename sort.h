f6f53d52a377f513c10c447f49f8d9b176336597
Clear008 <wissal-exo@hotmail.com>
2024-01-22 07:23:31 -0800

    0-bubble_sort.c

--- sort.h
+++ sort.h
@@ -25,7 +25,7 @@ struct listint_s *next;
 
 /* Helper functions */ 
 void print_array(const int *array, size_t size);
-void print_list(const listint_t *list)
+void print_list(const listint_t *list);
 
 /* tasks functions */
 void bubble_sort(int *array, size_t size);
